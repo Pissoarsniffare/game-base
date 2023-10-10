ea10cff5c560855b6ece56b2013948b417e5894e
Pissoarsniffare <94972482+Pissoarsniffare@users.noreply.github.com>
2023-10-10 09:38:51 +0200

    gjort klart steg 3

--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,5 @@
+import Player from './Player.js'
+
 export default class Game {
   constructor(width, height) {
     this.width = width
@@ -7,13 +9,17 @@ export default class Game {
     this.gameOver = false
     this.gravity = 1
     this.debug = false
+    this.player = new Player(this)
   }
 
   update(deltaTime) {
     if (!this.gameOver) {
       this.gameTime += deltaTime
+      this.player.update(deltaTime)
     }
   }
 
-  draw(context) {}
+  draw(context) {
+    this.player.draw(context)
+  }
 }
