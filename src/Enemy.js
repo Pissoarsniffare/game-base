994b84fad7b80527f14822cd3d00ef4811d67a4d
Pissoarsniffare <94972482+Pissoarsniffare@users.noreply.github.com>
2023-10-16 13:15:46 +0200

    gjort klart steg 7

--- src/Enemy.js
+++ src/Enemy.js
@@ -15,3 +15,16 @@ export default class Enemy {
       draw(context) {
         context.fillStyle = '#0f0'
         context.fillRect(this.x, this.y, this.width, this.height)
+
+
+        if (this.game.debug) {
+          context.strokeRect(this.x, this.y, this.width, this.height)
+          context.fillStyle = 'black'
+          context.font = '20px Arial'
+          context.fillText(this.lives, this.x, this.y - 5)
+          context.font = '12px Arial'
+          context.fillText(`x: ${this.x.toFixed()}`, this.x + 20, this.y - 5)
+          context.fillText(`y: ${this.y.toFixed()}`, this.x + 20, this.y - 20)
+        }
+      }
+    }
--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,4 @@
+import Slime from './Slime.js'
 import Player from './Player.js'
 import InputHandler from './InputHandler.js'
 import UserInterface from './UserInterface.js'
@@ -14,20 +15,39 @@ export default class Game {
     this.debug = false
     this.gameTime = 0
 
+    this.enemies = []
+    this.enemyTimer = 0
+    this.enemyInterval = 1000
+
     this.player = new Player(this)
+
   }
 
   update(deltaTime) {
     if (!this.gameOver) {
       this.gameTime += deltaTime
-
     }
     this.player.update(deltaTime)
+
+    if (this.enemyTimer > this.enemyInterval && !this.gameOver) {
+      this.addEnemy()
+      this.enemyTimer = 0
+    } else {
+      this.enemyTimer += deltaTime
+    }
+
+    this.enemies.forEach((enemy) => enemy.update(deltaTime))
+    this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion)
   }
 
   draw(context) {
     this.ui.draw(context)
     this.player.draw(context)
+    this.enemies.forEach((enemy) => enemy.draw(context))
+  }
+
+  addEnemy() {
+    this.enemies.push(new Slime(this))
   }
 }
 
